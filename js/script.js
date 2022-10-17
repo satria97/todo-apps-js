019a03d7e11d12a22c6ee0931cc2c4154c7fd819
satria97 <satria.code97@gmail.com>
2022-10-17 18:12:01 +0700

    Implementasi Web Storage

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ document.addEventListener("DOMContentLoaded", function () {
 		event.preventDefault();
 		addTodo();
 	});
+
+	if (isStorageExist()) {
+		loadDataFromStorage();
+	}
 });
 
 // Variabel todos adalah sebuah variabel berisi array yang akan menampung beberapa object.
@@ -187,4 +191,32 @@ function saveData() {
 		localStorage.setItem(STORAGE_KEY, parsed);
 		document.dispatchEvent(new Event(SAVED_EVENT));
 	}
+}
+
+const SAVED_EVENT = "saved-todo";
+const STORAGE_KEY = "TODO_APPS";
+
+function isStorageExist() /* boolean */ {
+	if (typeof Storage === undefined) {
+		alert("Browser kamu tidak mendukung local storage");
+		return false;
+	}
+	return true;
+}
+
+document.addEventListener(SAVED_EVENT, function () {
+	console.log(localStorage.getItem(STORAGE_KEY));
+});
+
+function loadDataFromStorage() {
+	const serializedData = localStorage.getItem(STORAGE_KEY);
+	let data = JSON.parse(serializedData);
+
+	if (data !== null) {
+		for (const todo of data) {
+			todos.push(todo);
+		}
+	}
+
+	document.dispatchEvent(new Event(RENDER_EVENT));
 }
